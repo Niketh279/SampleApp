227d8b1e0fbfd87cd893ef8f032c8827b2cb4d09
niketh.bandari <niketh.bandari@L0377D>
2019-10-24 16:58:27 +0530

    Comment Added Dev Checkin + UI Checkin

--- Sample_Application/Models/AccountViewModels.cs
+++ Sample_Application/Models/AccountViewModels.cs
@@ -14,7 +14,7 @@ namespace Sample_Application.Models
     {
         public string ReturnUrl { get; set; }
     }
-    //Comment Added Dev Checkin
+    //Comment Added Dev Checkin + UI Checkin
     public class SendCodeViewModel
     {
         public string SelectedProvider { get; set; }
